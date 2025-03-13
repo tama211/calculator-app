6a42c2ea2fa5c35d2f534c80b0abab73ed4c98e8
tama <adev@students.amikom.ac.id>
2025-03-13 10:10:32 +0700

    nambah lagi

--- Calculator.cs
+++ Calculator.cs
@@ -2,11 +2,17 @@ class Calculator
 {
 	static void main (string [] args)
 	{
-	
+	int a =10 ;
+	int b =6;
+	Console.WriteLine ("Hasil penambahan: {0} + {1} = {2}",a,b Penambahan(a, b);
+	Console.WriteLine ("Hasil penguranan: {0} - {1} = {2}",a,b Pengurangan(a, b);
 	}
 	static int penambahan (int a, int b)
 	{
 		return a+b;
 	}
-	
+	static int pengurangan (int a, int b)
+	{
+		return a-b;
+	}
 }
